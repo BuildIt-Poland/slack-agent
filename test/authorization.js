b9d8fd85218c1b316af5e3283f216d84299ad93f
bjowczarek <bjowczarek@gmail.com>
2019-01-28 16:22:28 +0100

    authorize method test

--- test/authorization.js
+++ test/authorization.js
@@ -3,8 +3,8 @@ var expect = require("chai").expect;
 var auth = require("../authorization.js");
 var moxios = require('moxios')
 
-describe("Really bad test", function () {
-    describe("Check authorize method", function () {
+describe("Authorization module tests", function () {
+    describe("Check authorize() method", function () {
         beforeEach(function () {
           // import and pass your custom axios instance to this method
           moxios.install()
@@ -16,26 +16,24 @@ describe("Really bad test", function () {
         })
 
         it("creates post that calls aprioprate url with required params", async function () {
-            let payload = {
-                code: "1111"
-            };
-            
             moxios.wait(function () {
                 let request = moxios.requests.mostRecent();
-                console.log(request)
                 request.respondWith({
                   status: 200,
                   response: 
                     { url: request.config.url, data: request.config.data},
                 })
             });
+            let payload = {
+                code: "1111"
+            };
             let url = await auth.authorize(payload);
             expect(url.config.url).to.equal("https://slack.com/api/oauth.access");
-            let contains = url.config.data.indexOf("1111");
-            expect(contains).not.equal(-1);
-            contains = url.config.data.indexOf("code");
-            expect(contains).not.equal(-1);
-            //TODO: tests for keys
+            expect(url.config.data.indexOf("1111")).not.equal(-1);
+            expect(url.config.data.indexOf("code")).not.equal(-1);
+            expect(url.config.data.indexOf("client_id")).not.equal(-1);
+            expect(url.config.data.indexOf("client_secret")).not.equal(-1);
+            
         });
     });
 });
