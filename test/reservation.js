41bf8d07203d25150aa9f37cda0d0d1144c3964a
Maciej Hein <maciej.hein@wipro.com>
2019-02-04 16:01:49 +0100

    tests

--- test/reservation.js
+++ test/reservation.js
@@ -1,1 +1,17 @@
+'use strict';
 
+/* global describe it  */
+const expect = require('chai').expect;
+const reservation = require('../workers/reservation.js');
+
+describe('Reservation module tests',  () => {
+	describe('Check findReservationByDate(date, tableName)', () => {
+		it('returns reservation for specific date', async () => {
+			expect(`Test`).to.equal(`Test`);
+		});
+		it(`returns undefined when reservations isn't exist in database`, async () => {
+
+		});
+	});
+
+});
