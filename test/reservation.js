1bcf9cf50889dbf00b139bcadac6ccf56bc605ec
Maciej Hein <maciej.hein@wipro.com>
2019-02-04 16:20:31 +0100

    fix

--- test/reservation.js
+++ test/reservation.js
@@ -2,16 +2,33 @@
 
 /* global describe it  */
 const expect = require('chai').expect;
-const reservation = require('../workers/reservation.js');
 
 describe('Reservation module tests',  () => {
 	describe('Check findReservationByDate(date, tableName)', () => {
 		it('returns reservation for specific date', async () => {
-			expect(`Test`).to.equal(`Test`);
 		});
 		it(`returns undefined when reservations isn't exist in database`, async () => {
-
 		});
 	});
 
 });
+
+/*
+var updateTableSpy = sinon.spy();
+AWS.mock('DynamoDB', 'updateTable', updateTableSpy);
+ 
+// Object under test
+myDynamoManager.scaleDownTable();
+ 
+// Assert on your Sinon spy as normal
+assert.isTrue(updateTableSpy.calledOnce, 'should update dynamo table via AWS SDK');
+var expectedParams = {
+  TableName: 'testTableName',
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  }
+};
+assert.isTrue(updateTableSpy.calledWith(expectedParams), 'should pass correct parameters');
+
+*/
