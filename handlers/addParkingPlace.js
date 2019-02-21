5b53db8283d14e97326f275a5b9e893b82d82745
Marcin Goławski <marcin.golawski@wipro.com>
2019-02-21 12:37:38 +0100

    Small refactor for handling server responses (#21)

--- handlers/addParkingPlace.js
+++ handlers/addParkingPlace.js
@@ -1,44 +1,39 @@
 const auth = require('../security/authorization.js');
+const parkingPlace = require('../workers/parkingPlace.js');
+const { success, unauthorized } = require('../utility/reponseBuilder.js');
 const { isCity } = require('../utility/requestValidator.js');
 const { parseBodyToObject } = require('../utility/requestParser.js');
 const { generateResponseBody } = require('../utility/responseBody.js');
-const parkingPlace = require('../workers/parkingPlace.js');
 
 const { SIGNING_SECRET, ENV_STAGE, TABLE_NAME } = require('../config/all.js');
 
 module.exports.parkingPlace = async event => {
-  const isValid = await auth.isVerified(event, SIGNING_SECRET, ENV_STAGE);
-  if (!isValid)
-    return {
-      statusCode: 401,
-    };
+  const isVerified = await auth.isVerified(event, SIGNING_SECRET, ENV_STAGE);
+  if (!isVerified) {
+    return unauthorized();
+  }
+
   const { message, isValidCommand } = parseBodyToObject(event, {
     city: {
       required: city => !!city,
-      pattern: city => isCity(city),
+      pattern: isCity,
     },
     place: {
       required: date => !!date
     },
   });
 
-  if (!isValidCommand)
-    return {
-      statusCode: 200,
-      body: generateResponseBody(message),
-    };
+  if (!isValidCommand){
+    return success(generateResponseBody(message));
+  }
 
   const result = await parkingPlace.saveParkingPlace(message, TABLE_NAME);
 
-  return result
-    ? {
-        statusCode: 200,
-        body: generateResponseBody(
-          `You added a parking place.\n *City:* ${message.city}\n *Place:* ${message.place}`,
-        ),
-      }
-    : {
-        statusCode: 200,
-        body: generateResponseBody(`You can't add parking place`),
-      };
+  if(result) {
+    return success(generateResponseBody(
+      `You added a parking place.\n *City:* ${message.city}\n *Place:* ${message.place}`,
+    ));
+  }
+
+  return success(generateResponseBody(`You can't add parking place`));
 };
