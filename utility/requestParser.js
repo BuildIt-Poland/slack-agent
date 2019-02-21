6851fe1ec7cd0877d7346e05f2bad3c818f1c2ed
Marcin Goławski <marcin.golawski@wipro.com>
2019-02-21 15:20:41 +0100

    Fixes for requestParser

--- utility/requestParser.js
+++ utility/requestParser.js
@@ -1,24 +1,34 @@
 const _ = require('lodash');
 const queryString = require('query-string');
 
-const isTextPropertyValid = (textProperty, validationForProperty) => {
-  return  _.every(_.values(validationForProperty), validation => validation(textProperty));
-}
+const isParamValid = (inputParam, paramValidators) =>
+  _.every(_.values(paramValidators), validate => validate(inputParam));
 
-const isTextValid = (text, validations) => {
-  return _.reduce(text, (isObjectValid, userInputProperty, propertyKey) => 
-    (isTextPropertyValid(userInputProperty, validations[propertyKey]) && isObjectValid), true);
-}
+const validateInputParams = (inputParams, inputFormat) =>
+  _.reduce(
+    inputParams,
+    (isValid, userInputProperty, propertyKey) =>
+      isParamValid(userInputProperty, inputFormat[propertyKey]) && isValid,
+    true,
+  );
 
-exports.parseBodyToObject = (body, textFormat) => {
+exports.parseBodyToObject = (body, inputFormat) => {
   const parsedBody = queryString.parse(body);
-  const textArray = _.split(parsedBody.text, ' ');
-  const text = _.reduce(_.keys(textFormat), (acc, key, index) => 
-    ({...acc, [key]: textArray[index]}), 
-    _.has(textFormat, 'userName') ? { userName: parsedBody.user_name } : {});
-  const isValid = isTextValid(text, textFormat);
+  const inputParamsList = _.split(parsedBody.text, ' ');
+
+  const inputParams = _.reduce(
+    _.keys(inputFormat),
+    (params, paramName, index) => ({ ...params, [paramName]: inputParamsList[index] }),
+    {},
+  );
+
+  if (_.has(inputFormat, 'userName')) {
+    inputParams.userName = parsedBody.user_name;
+  }
+
+  const isValid = validateInputParams(inputParams, inputFormat);
   return {
     isValid,
-    message: isValid ? text : 'Invalid command'
-  }
-}
+    message: isValid ? inputParams : 'Invalid command',
+  };
+};
