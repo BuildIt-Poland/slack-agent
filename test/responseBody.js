960a19181fd3546cb1cb89312d1b7dd8b43534ad
Maciej Hein <maciej.hein@wipro.com>
2019-02-25 12:33:17 +0100

    change test to responseBody

--- test/responseBody.js
+++ test/responseBody.js
@@ -3,25 +3,37 @@ const { expect } = require('chai');
 const { generateResponseBody, generateResponseBodyWithAttachments } = require('../utility/responseBody.js');
 
 describe('responseBody module tests', () => {
-  describe('Check generateResponseBody(text) function', () => {
-    it('returns body with text', () => {
-        const body = generateResponseBody(`You can't add parking place`);
-        const bodyJson = JSON.parse(body);
-        expect(bodyJson).to.haveOwnProperty('text', `You can't add parking place`);
-    });
-  });
-  describe('Check generateResponseBodyWithAttachments(title, attachments) function', () => {
-    it('returns body with title and attachments', () => {
-        const body = generateResponseBodyWithAttachments('Places:', [
-            {
-                city: 'Gdansk',
-                place: 11
-            }
-        ]);
-        const bodyJson = JSON.parse(body);
-        expect(bodyJson).to.haveOwnProperty('text', 'Places:');
-        expect(bodyJson).to.haveOwnProperty('attachments');
-        expect(bodyJson.attachments[0]).to.haveOwnProperty('text', `*city:* Gdansk\n*place:* 11\n`);
-    });
-  });
+	describe('Check generateResponseBody(text) function', () => {
+		it('returns body with text', () => {
+			const body = generateResponseBody(`You can't add parking place`);
+			const bodyJson = JSON.parse(body);
+			expect(bodyJson).to.haveOwnProperty('text', `You can't add parking place`);
+		});
+	});
+	describe('Check generateResponseBodyWithAttachments(title, attachments) function', () => {
+		it('returns body with title and attachments', () => {
+			const body = generateResponseBodyWithAttachments('Places:', [
+				{
+					city: 'Gdansk',
+					place: 11
+				}
+			]);
+			const bodyJson = JSON.parse(body);
+			expect(bodyJson).to.haveOwnProperty('text', 'Places:');
+			expect(bodyJson).to.haveOwnProperty('attachments');
+			expect(bodyJson.attachments[0]).to.haveOwnProperty('text', `*city:* Gdansk\n*place:* 11\n`);
+		});
+		it('returns body with title and attachments when body parameter is null', () => {
+			const body = generateResponseBodyWithAttachments('Places:', [
+				{
+					city: 'Gdansk',
+					place: null
+				}
+			]);
+			const bodyJson = JSON.parse(body);
+			expect(bodyJson).to.haveOwnProperty('text', 'Places:');
+			expect(bodyJson).to.haveOwnProperty('attachments');
+			expect(bodyJson.attachments[0]).to.haveOwnProperty('text', `*city:* Gdansk\n`);
+		});
+	});
 });
