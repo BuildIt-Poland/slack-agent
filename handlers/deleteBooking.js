48e698c3d12955971f4df6455ef62d5cd74e9386
Maciej Hein <maciej.hein@wipro.com>
2019-02-27 16:14:36 +0100

    delete handler refactor

--- handlers/deleteBooking.js
+++ handlers/deleteBooking.js
@@ -1,15 +1,15 @@
-const auth = require('../security/authorization.js');
-const { findReservationByDate, deleteReservationPlace } = require('../dao/bookings.js');
-const { success, internalServerError, unauthorized } = require('../utilities/reponseBuilder.js');
+const _ = require('lodash');
+const { isVerified } = require('../security/authorization.js');
+const { unbookParkingPlace } = require('../dao/bookings.js');
+const { success, unauthorized, internalServerError } = require('../utilities/reponseBuilder.js');
 const { isCity, isFutureDate } = require('../utilities/requestValidator.js');
 const { parseBodyToObject } = require('../utilities/requestParser.js');
 const { generateResponseBody } = require('../utilities/responseBody.js');
 
-const { SIGNING_SECRET, ENV_STAGE, PARKING_PLACES_TABLE } = require('../config/all.js');
+const { SIGNING_SECRET, ENV_STAGE } = require('../config/all.js');
 
-module.exports.deleteReservation = async (event) => {
-  const isVerified = await auth.isVerified(event, SIGNING_SECRET, ENV_STAGE);
-  if (!isVerified) {
+module.exports.delete = async (event) => {
+  if (!await isVerified(event, SIGNING_SECRET, ENV_STAGE)) {
     return unauthorized();
   }
 
@@ -29,15 +29,15 @@ module.exports.deleteReservation = async (event) => {
     return success(generateResponseBody(message));
   }
 
-  const reservation = await findReservationByDate(message.dates, PARKING_PLACES_TABLE);
-  if (!reservation) {
-    return internalServerError();
-  }
+  const { dates, city, userName } = message;
 
-  const placeDeleted = await deleteReservationPlace(reservation, message, PARKING_PLACES_TABLE);
-  if (!placeDeleted) {
-    return success(generateResponseBody(`You don't have reservation`));
-  }
+  const bookingPromises = _.map([ dates ], async (bookingDate) => {
+    return unbookParkingPlace(bookingDate, city, userName);
+  });
+
+  await Promise.all(bookingPromises).catch(() => {
+    return internalServerError();
+  });
 
   return success(generateResponseBody(`Reservation deleted`));
 };
