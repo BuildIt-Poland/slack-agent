baefe849a41d78640332cf27e01a94bbbf3b24f1
Maciej Hein <maciej.hein@wipro.com>
2019-02-27 16:21:22 +0100

    addParkingPlace small refactor

--- dao/parkingPlace.js
+++ dao/parkingPlace.js
@@ -1,32 +1,25 @@
 const { query, save } = require('../services/dbService.js');
-const { error } = require('../services/loggerService.js');
 
 const { PARKING_PLACES_TABLE } = require('../config/all');
 
 exports.addParkingPlace = async (userInputParams) => {
-  try {
-    await save(
-      {
-        City: userInputParams.city,
-        PlaceID: userInputParams.placeId
-      },
-      PARKING_PLACES_TABLE
-    );
-    return true;
-  } catch (e) {
-    error('reservation.addParkingPlace', e);
-    return false;
-  }
+  return save(
+    {
+      City: userInputParams.city,
+      PlaceID: userInputParams.placeId
+    },
+    PARKING_PLACES_TABLE
+  );
 };
 
 exports.getParkingPlaces = async (city) => {
   const params = {
     KeyConditionExpression: 'City=:city',
     ExpressionAttributeValues: {
-      ':city': city,
-    },
+      ':city': city
+    }
   };
 
   const { Items } = await query(params, PARKING_PLACES_TABLE);
   return Items;
-}
+};
