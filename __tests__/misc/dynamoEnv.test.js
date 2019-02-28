b64754ac178d8fdfe2abb7070c4e82f5148bab3b
Marcin Goławski <marcin.golawski@wipro.com>
2019-02-28 14:37:14 +0100

    Fix authService.test.js

--- __tests__/misc/dynamoEnv.test.js
+++ __tests__/misc/dynamoEnv.test.js
@@ -1,31 +1,28 @@
-
-/* global describe it beforeEach afterEach */
 const dynamoEnv = require('../../config/dynamoEnv.js');
 
-describe('dynamoEnv module tests', () => {
-  let val;
-  describe('Check awsEnv() function', () => {
-    beforeEach(() => {
-      val = process.env.LOCAL_DB;
-    });
+const { LOCAL_DB } = process.env;
 
+describe('dynamoEnv module tests', () => {
+  describe('Check awsEnv() function', () => {
     afterEach(() => {
-      process.env.LOCAL_DB = val;
+      process.env.LOCAL_DB = LOCAL_DB;
     });
 
     it('returns expected params for testing', async () => {
       process.env.LOCAL_DB = 'true';
       const params = dynamoEnv.awsEnv();
-      expect(params.endpoint).to.equal('http://dynamodb:8000');
-      expect(typeof params.credentials).to.equal('object');
-      expect(params.region).to.equal('us-east-1');
+
+      expect(params.endpoint).toBe('http://dynamodb:8000');
+      expect(params.credentials).toBeDefined();
+      expect(params.region).toBe('us-east-1');
     });
     it('returns expected params for prod', async () => {
       delete process.env.LOCAL_DB;
       const params = dynamoEnv.awsEnv();
-      expect(typeof params.endpoint).to.equal('undefined');
-      expect(typeof params.credentials).to.equal('undefined');
-      expect(typeof params.region).to.equal('undefined');
+
+      expect(params.endpoint).toBeUndefined();
+      expect(params.credentials).toBeUndefined();
+      expect(params.region).toBeUndefined();
     });
   });
 });
