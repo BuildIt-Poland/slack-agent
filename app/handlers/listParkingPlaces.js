1854ba4336ad2159d3f27c2b93b9550d349a3774
maciejhein <45594475+maciejhein@users.noreply.github.com>
2019-03-06 15:13:40 +0100

    Bugfix/list parking places (#38)
    * list parking spaces when booking doesn't exists
    
    * fix

--- app/handlers/listParkingPlaces.js
+++ app/handlers/listParkingPlaces.js
@@ -1,30 +1,31 @@
 const _ = require('lodash');
 const { isVerified } = require('../services/authService.js');
 const { getBooking } = require('../dao/bookings.js');
+const { getFreeParkingPlacesMap } = require('../dao/parkingPlace.js');
 const { success, unauthorized } = require('../utilities/reponseBuilder.js');
 const {
   generateResponseBody,
-  generateResponseBodyWithAttachments
+  generateResponseBodyWithAttachments,
 } = require('../utilities/responseBody.js');
 const { isCity } = require('../utilities/requestValidator.js');
 const { parseBodyToObject } = require('../utilities/requestParser.js');
 
 const { ENV_STAGE, SIGNING_SECRET } = require('../../config/all.js');
 
-module.exports.list = async (event) => {
-  if (!await isVerified(event, SIGNING_SECRET, ENV_STAGE)) {
+module.exports.list = async event => {
+  if (!(await isVerified(event, SIGNING_SECRET, ENV_STAGE))) {
     return unauthorized();
   }
 
   const { message, isValid } = parseBodyToObject(event.body, {
     dates: {
-      required: (dates) => !_.isEmpty(dates)
+      required: dates => !_.isEmpty(dates),
     },
     city: {
       pattern: isCity,
-      required: (bookingDate) => !!bookingDate
+      required: bookingDate => !!bookingDate,
     },
-    userName: {}
+    userName: {},
   });
 
   if (!isValid) {
@@ -32,16 +33,17 @@ module.exports.list = async (event) => {
   }
 
   const { city, dates } = message;
-  const booking = await getBooking(dates[0], city);
 
-  if (_.isEmpty(booking)) {
-    return success(generateResponseBody(`Parking places don't exists`));
+  let { Places: parkingPlaces } = await getBooking(dates[0], city);
+
+  if (_.isEmpty(parkingPlaces)) {
+    parkingPlaces = await getFreeParkingPlacesMap;
   }
 
   return success(
     generateResponseBodyWithAttachments(
       'List of reservations with available places:',
-      booking.Places
-    )
+      parkingPlaces,
+    ),
   );
 };
